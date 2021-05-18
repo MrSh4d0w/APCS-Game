bfbc6ff578aa818f35a7afcfaed6ae7396dde489
WildRage <arg100@gmail.com>
2021-05-18 10:08:18 -0400

    Add setStatus method to Player.java

--- player/Player.java
+++ player/Player.java
@@ -12,10 +12,10 @@ public class Player {
         this.s = s;
     }
 
-    private int getLocationX(){return locX;}// returns location of Player object
-    private int getLocationY(){return locY;}// returns location of Player object
-    private void setLocationX(int x){locX = x;}// sets location of Player object
-    private void setLocationY(int y){locY = y;}// sets location of Player object
+    private int getLocationX(){return locX;}// returns location of Player object x
+    private int getLocationY(){return locY;}// returns location of Player object y
+    private void setLocationX(int x){locX = x;}// sets location of Player object x
+    private void setLocationY(int y){locY = y;}// sets location of Player object y
 
     private int getHP(){return HP;}// returns HP of Player object
     private void setHP(int HP){this.HP = HP;}// sets HP of Player object
@@ -23,4 +23,5 @@ public class Player {
     private void addHP(int x){HP+=x;}// adds x HP of Player object
 
     private status getStatus(){return s;}// returns status of Player object
+    private void setStatus(status s){this.s = s;}
 }
