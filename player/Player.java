bf947b2dce11f803e20e343f8dc22a7c59698c52
WildRage <arg100@gmail.com>
2021-05-18 10:04:54 -0400

    Format Player.java

--- player/Player.java
+++ player/Player.java
@@ -12,33 +12,15 @@ public class Player {
         this.s = s;
     }
 
-    private int getLocationX(){
-        return locX;
-    }// returns location of Player object
-    private int getLocationY(){
-        return locY;
-    }// returns location of Player object
-    private void setLocationX(int x){
-        locX = x;
-    }// sets location of Player object
-    private void setLocationY(int y){
-        locY = y;
-    }// sets location of Player object
+    private int getLocationX(){return locX;}// returns location of Player object
+    private int getLocationY(){return locY;}// returns location of Player object
+    private void setLocationX(int x){locX = x;}// sets location of Player object
+    private void setLocationY(int y){locY = y;}// sets location of Player object
 
-    private int getHP(){
-        return HP;
-    }// returns HP of Player object
-    private void setHP(int HP){
-        this.HP = HP;
-    }// sets HP of Player object
-    private void removeHP(int x){
-        HP-=x;
-    }// removes x HP of Player object
-    private void addHP(int x){
-        HP+=x;
-    }// adds x HP of Player object
+    private int getHP(){return HP;}// returns HP of Player object
+    private void setHP(int HP){this.HP = HP;}// sets HP of Player object
+    private void removeHP(int x){HP-=x;}// removes x HP of Player object
+    private void addHP(int x){HP+=x;}// adds x HP of Player object
 
-    private status getStatus(){
-        return s;
-    }// returns status of Player object
+    private status getStatus(){return s;}// returns status of Player object
 }
