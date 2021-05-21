73488d84f139ea722c65549953d16881806062a4
WildRage <arg100@gmail.com>
2021-05-21 14:26:22 -0400

    Add and resize image for test

--- major.java
+++ major.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
 public class major {
     public static void main(String[] args) {
         JFrame fram = new JFrame();//makes JFrame
-        fram.setSize(1920, 1080);//sets JFrame size
+        fram.setSize(256, 256);//sets JFrame size
+        fram.setUndecorated(true);
         fram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exits program on JFrame close
         fram.getContentPane().setBackground(new Color(50, 50, 50));//sets background color of JFrame
 
@@ -12,14 +17,30 @@ public class major {
         layeredPane.setPreferredSize(new Dimension(300, 310));//sets size of JLayeredPane
         layeredPane.setLayout(null);
 
-        majorPain m1 = new majorPain("screenshot.png");//makes new majorPain object
-        m1.setBounds(100, 200, 200, 100);//puts m1 in pos x,y and of size width,height
-        m1.setLocation(500, 500);//sets location of m1
+        majorPain c1 = new majorPain("levels/Level-0.png");
+        c1.setBounds(0,0, 256, 256);
+        layeredPane.add(c1, -1);
 
-        layeredPane.add(m1, 1);//makes house image at z of 0 in JLayeredPane
-        layeredPane.add(new majorPain("house.png"), 0);//makes otter image at z of 1 in JLayeredPane
+
+        BufferedImage p1 = null;
+        try {
+            p1 = ImageIO.read(new File("AllCharacters_Idle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Image subSprite = p1.getSubimage(0, 0, 78, 78);
+        majorPain p2 = new majorPain(subSprite);
+        p2.setBounds(96, 96, 78, 78); 
+        layeredPane.add(p2, 0);
+    
+
+        // majorPain m1 = new majorPain("screenshot.png");//makes new majorPain object
+        // m1.setBounds(100, 200, 200, 100);//puts m1 in pos x,y and of size width,height
+
+        // layeredPane.add(m1, 1);//makes house image at z of 0 in JLayeredPane
+        // layeredPane.add(new majorPain("house.png"), 0);//makes otter image at z of 1 in JLayeredPane
+        
         fram.add(layeredPane);//adds JLayeredPane to JFrame
-
         fram.setResizable(false);
         fram.setVisible(true); //makes JFrame visible
     }
