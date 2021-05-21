6e29de2c43cce913e050233f26fda95037a45701
WildRage <arg100@gmail.com>
2021-05-21 10:15:06 -0400

    Implement setBounds() to major

--- major.java
+++ major.java
@@ -10,9 +10,14 @@ public class major {
 
         JLayeredPane layeredPane = new JLayeredPane();//makes new JLayeredPane (only one)
         layeredPane.setPreferredSize(new Dimension(300, 310));//sets size of JLayeredPane
+        layeredPane.setLayout(null);
 
-        layeredPane.add(new majorPain("house.png"), 1);//makes house image at z of 0 in JLayeredPane
-        layeredPane.add(new majorPain("otter.png"), 0);//makes otter image at z of 1 in JLayeredPane
+        majorPain m1 = new majorPain("screenshot.png");//makes new majorPain object
+        m1.setBounds(100, 200, 200, 100);//puts m1 in pos x,y and of size width,height
+        m1.setLocation(500, 500);//sets location of m1
+
+        layeredPane.add(m1, 1);//makes house image at z of 0 in JLayeredPane
+        layeredPane.add(new majorPain("house.png"), 0);//makes otter image at z of 1 in JLayeredPane
         fram.add(layeredPane);//adds JLayeredPane to JFrame
 
         
