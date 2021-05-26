6cfdafb2d85a7feb32d7be7a6ef46632ddc494d1
WildRage <arg100@gmail.com>
2021-05-26 10:17:29 -0400

    Test setLocation

--- GameRunner.java
+++ GameRunner.java
@@ -7,18 +7,18 @@ public class GameRunner{
     private static console c;
 
     public static void createGrid() {
-        grid = new String[8][12];
+        grid = new String[13][9];
         int rowCoords = 0;
         int columnCoords = 0;
-        for (int row = 0; row < 8; row++) {
+        for (int row = 0; row < 13; row++) {
             rowCoords = (row*112);
-            for (int column = 0; column < 12; column++) {
+            for (int column = 0; column < 9; column++) {
                 columnCoords = (column*112);
                 grid[row][column] = rowCoords + " " + columnCoords;
             }   
         }
     }
-
+    
     public static void main(String[] args) {
         createGrid();
 
@@ -40,7 +40,7 @@ public class GameRunner{
 
         p = new Player(100, "ASSAULT", 50, 20);//*Player
         p.setSize(new Dimension(112, 112));
-        p.setLocation(grid[1][2]);
+        p.setLocation(grid[12][8]);
         mainPanel.add(p, 0);
         
 
