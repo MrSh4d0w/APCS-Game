29da38739fec75e33800832209f93738db2cef76
WildRage <arg100@gmail.com>
2021-05-27 19:04:48 -0400

    Extract ActionListener to method for optimization

--- console.java
+++ console.java
@@ -30,41 +30,16 @@ public class console extends JPanel implements ActionListener{
         
 
         
-        
-        
+        textArea = new JTextArea(5, 20);
+            textAreaInitializer();
+        label = new JLabel(">", JLabel.LEFT);
+            labelInitializer();
         textField = new JTextField(1);
-        ActionListener l = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String txt = textField.getText();
-                textArea.insert(" "+txt, 0);
-                
-                if(includes(txt, "move to")){
-                    String[] args = parser("move to");
-                    if(args==null){System.exit(0);}
-                    try{
-                        int y = Character.getNumericValue(args[0].charAt(1));
-                        int x = (Character.getNumericValue(args[0].charAt(0)))-9;
-                        GameRunner.setLocation(x*112, y*112);
-                        System.out.println(x + " " + y);
-                    } catch (Exception ex){
-                        System.out.println("NumberFormatException");
-                    }        
-                }
-                if(includes(txt, "help")){
-                    textArea.insert("\ngo fuck yourself\n", 5);
-                }
-                if(txt.equalsIgnoreCase("exit") || txt.equalsIgnoreCase("close")) {
-                    System.exit(0);
-                }
-                if(includes(txt, "clear")){textArea.setText(null);} else {textArea.insert("\n>", 0);}
-                textField.setText("");
-            }
-        };
-        
+            textFieldInitializer();
         setFonts();
-        textFieldInitializer(l);
-        textAreaInitializer();
-        labelInitializer();
+        
+        
+        
         this.add(textField);
         this.add(textArea);
         this.add(label);
@@ -73,7 +48,31 @@ public class console extends JPanel implements ActionListener{
     public void paintComponent(Graphics g) {
         g.drawImage(decorations, 0, 0, null);
     }
-    public void actionPerformed(ActionEvent e) {/*gamer*/}
+    public void actionPerformed(ActionEvent e) {
+        String txt = textField.getText();
+            textArea.insert(" "+txt, 0);
+            
+            if(includes(txt, "move to")){
+                String[] args = parser("move to");
+                if(args==null){System.exit(0);}
+                try{
+                    int y = Character.getNumericValue(args[0].charAt(1));
+                    int x = (Character.getNumericValue(args[0].charAt(0)))-9;
+                    GameRunner.setLocation(x*112, y*112);
+                    System.out.println(x + " " + y);
+                } catch (Exception ex){
+                    System.out.println("NumberFormatException");
+                }        
+            }
+            if(includes(txt, "help")){
+                textArea.insert("\ngo fuck yourself\n", 5);
+            }
+            if(txt.equalsIgnoreCase("exit") || txt.equalsIgnoreCase("close")) {
+                System.exit(0);
+            }
+            if(includes(txt, "clear")){textArea.setText(null);} else {textArea.insert("\n>", 0);}
+            textField.setText("");
+    }
     
     private String[] parser(String command){
         String txt = textField.getText().replace(",", "");
@@ -83,24 +82,23 @@ public class console extends JPanel implements ActionListener{
         return txt.substring(command.length()).trim().split("\s");
     }
     private void labelInitializer() {
-        label = new JLabel(">", JLabel.LEFT);
         label.setForeground(notWhite);
         label.setBackground(notBlack);
         label.setSize(20, 30);
         label.setOpaque(true);
         label.setLocation(0, 0);
     }
-    private void textFieldInitializer(ActionListener l) {
+    private void textFieldInitializer() {
         textField.setBackground(notBlack);
         textField.setForeground(notWhite);
-        textField.addActionListener(l);
+        textField.addActionListener(this);
         textField.setSize(444, 30);
         textField.setLocation(20, 0);
         textField.setCaretColor(notWhite);
         textField.setBorder(BorderFactory.createLineBorder(notBlack));
     }
     private void textAreaInitializer() {
-        textArea = new JTextArea(5, 20);
+        
         textArea.setSize(464, 1060);
         textArea.setBackground(notBlack);
         textArea.setEditable(false);      
