368d81f52cca491eb4318275b2a511787834ef57
WildRage <arg100@gmail.com>
2021-05-27 10:03:08 -0400

    Import sprite sheet

--- Player.java
+++ Player.java
@@ -78,7 +78,7 @@ public class Player extends JPanel implements ActionListener{
 
     public Graphics animateASSAULT(Graphics g){
         getImg("Character1_Gun");
-        resizeImg(0, 0, 96, 96);
+        resizeImg(0, 0, 112, 112);
         g.drawImage(img, 0, 0, null);
         return g;
     }// !Do this
