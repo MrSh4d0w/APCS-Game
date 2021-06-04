89ecdb2f44e8bd764a214557161073d1142a1fe3
MrSh4d0w <chromestrikeyt@gmail.com>
2021-06-04 15:50:53 -0400

    Titlescreen

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,3 +1,31 @@
+import java.awt.*;
+import java.awt.Graphics.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 public class TitleScreen {
     
+    private static Image img = new ImageIcon("images/Titlescreen.png").getImage();
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JPanel p = new JPanel() {
+            public void paintComponent(Graphics g) {
+                g.drawImage(img, 0, 0, null);
+            }
+        };
+        f.setSize(1920, 1080);
+        f.setUndecorated(true);
+        f.add(p);
+        f.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent ke) {
+                if(ke.getKeyCode() == KeyEvent.VK_LEFT){System.out.println("helllo");};
+            }
+            public void keyPressed(KeyEvent ke) {
+                if(ke.getKeyCode() == KeyEvent.VK_SPACE){GameRunner.main(null);};
+            }
+            public void keyReleased(KeyEvent ke) {}
+        });
+        f.setVisible(true);
+    }
 }
